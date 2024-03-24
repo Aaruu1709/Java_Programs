728c3cb00706db3b5a9f939259133009b1d2efb4
Shinde Arti <102209037+Aaruu1709@users.noreply.github.com>
2024-03-24 12:52:59 +0530

    adding more java files

---
+++ number.java
@@ -0,0 +1,15 @@
+public class number {
+    public static void main(String[] args) {
+
+        // 1
+        // 1 2
+        // 1 2 3
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+}
